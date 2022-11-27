c5c065906b424db427c660586a3b401c5ad0cb67
masatotezuka <tez.0731.tez@gmail.com>
2022-11-27 15:40:00 +0900

    add:ToastContainer

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -1,16 +1,16 @@
+import 'react-toastify/dist/ReactToastify.css';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import './styles.css';
+import { StyledToastContainer } from '../components';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to client!</title>
+        <title>isms management</title>
       </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
+      <StyledToastContainer />
+      <Component {...pageProps} />
     </>
   );
 }
