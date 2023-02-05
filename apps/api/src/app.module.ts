6885cfbba77fd3dc094175427c2c9fd1afb43616
masatotezuka <tez.0731.mst@gmail.com>
2023-02-05 10:12:02 +0900

    add: machine module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 
 import { AuthModule } from './auth/auth.module';
 import { AdminModule } from './admin/admin.module';
+import { MachineModule } from './machine/machine.module';
 
 @Module({
-  imports: [AuthModule, AdminModule],
+  imports: [AuthModule, AdminModule, MachineModule],
 })
 export class AppModule {}
