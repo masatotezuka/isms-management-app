e82994500289ae247e70a47d08dfa9f37fd85fd9
masatotezuka <tez.0731.mst@gmail.com>
2023-02-05 10:13:43 +0900

    add: ValidationPipe

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,7 +3,7 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
@@ -14,6 +14,8 @@ async function bootstrap() {
   });
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  //string型をnumber型に変換を許可する
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
